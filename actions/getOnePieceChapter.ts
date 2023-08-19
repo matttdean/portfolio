eb1ab8479ceed1884927442f9ed2373b8de26312
Matthew Dean <37552721+matttdean@users.noreply.github.com>
2023-08-19 17:55:22 -0400

    add vercel.json for CORS configuration

--- actions/getOnePieceChapter.ts
+++ actions/getOnePieceChapter.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const baseUrl = 'https://api.mangadex.org';
-const title = 'One Piece';
 const mangaID = 'a1c7c817-4e59-43b7-9365-09675a149a6f'
 
 export const getLatestOnePieceChapter = async () => {
