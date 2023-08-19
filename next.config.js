38e4b2ed31906764e800f6824428531bea09f3a7
Matthew Dean <37552721+matttdean@users.noreply.github.com>
2023-08-19 18:05:34 -0400

    add correct source to vercel.json

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/manga/a1c7c817-4e59-43b7-9365-09675a149a6f/feed?order[chapter]=desc&limit=1',
-            destination: 'https://api.mangadex.org/manga/a1c7c817-4e59-43b7-9365-09675a149a6f/feed?order[chapter]=desc&limit=1',
-          },
-        ]
-      },
     images: {
         remotePatterns: [
             {
