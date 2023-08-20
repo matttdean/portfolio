d0090f6f469971a0fa38f2b6917079350d083561
Matthew Dean <37552721+matttdean@users.noreply.github.com>
2023-08-20 00:37:14 -0400

    add rewrites to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api.example.com/:path*',
+          },
+        ]
+      },
     async headers() {
         return [
             {
