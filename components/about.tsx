a9d33d8f4b582d2e06ad3c4eb2a9553612d2842c
Matthew Dean <37552721+matttdean@users.noreply.github.com>
2023-08-20 00:45:30 -0400

    move getLatestOnePieceChapter to About

--- components/about.tsx
+++ components/about.tsx
@@ -4,7 +4,21 @@ import React, { useEffect, useState } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { getLatestOnePieceChapter } from "@/api/getOnePieceChapter";
+
+import axios from 'axios';
+
+const baseUrl = 'https://api.mangadex.org';
+const mangaID = 'a1c7c817-4e59-43b7-9365-09675a149a6f'
+
+export const getLatestOnePieceChapter = async () => {
+    return await axios({
+        method: 'GET',
+        url: `${baseUrl}/manga/${mangaID}/feed?order[chapter]=desc&limit=1`,
+    });
+}
+
+
+
 
 export default function About() {
   const { ref } = useSectionInView("About", 1);
